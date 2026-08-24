cd52824add92e49426bd8f2891df7cf9c94d13d5
Deniskaaaz <www.petuk-denis@yandex.ru>
2026-08-24 21:57:34 +0000

    Remove unnecessary ExperimentalMaterialApi annotation

--- app/src/main/java/com/tripbot/app/ui/screens/TripDetailScreen.kt
+++ app/src/main/java/com/tripbot/app/ui/screens/TripDetailScreen.kt
@@ -1,7 +1,5 @@
-@file:OptIn(ExperimentalMaterialApi::class)
 package com.tripbot.app.ui.screens
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.foundation.layout.padding
 
 import androidx.compose.material.icons.Icons
