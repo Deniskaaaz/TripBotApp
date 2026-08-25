0991b4b5b967d8cb2fc865608750285eeb40890a
Deniskaaaz <www.petuk-denis@yandex.ru>
2026-08-25 04:42:26 +0000

    Remove Java MainActivity and add Compose MainActivity

--- app/src/main/java/com/tripbot/app/MainActivity.kt
+++ app/src/main/java/com/tripbot/app/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.tripbot.app.ui.screens.CreateTripScreen
-import com.tripbot.app.ui.screens.TripDetailScreen
+import androidx.compose.ui.tooling.preview.Preview
 import com.tripbot.app.ui.screens.TripListScreen
 
 class MainActivity : ComponentActivity() {
@@ -19,40 +15,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                TripBotApp()
-            }
-        }
-    }
-}
-
-@Composable
-fun TripBotApp() {
-    val navController = rememberNavController()
-    NavHost(navController, startDestination = "trips") {
-        composable("trips") {
-            TripListScreen(
-                onTripClick = { tripId ->
-                    navController.navigate("detail/$tripId")
-                },
-                onAddTrip = {
-                    navController.navigate("create")
+                Surface {
+                    TripListScreen()
                 }
-            )
-        }
-        composable(
-            route = "detail/{tripId}",
-            arguments = listOf(navArgument("tripId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val tripId = backStackEntry.arguments?.getInt("tripId") ?: 0
-            TripDetailScreen(
-                tripId = tripId,
-                onBack = { navController.popBackStack() }
-            )
-        }
-        composable("create") {
-            CreateTripScreen(
-                onTripCreated = { navController.popBackStack() }
-            )
+            }
         }
     }
 }
