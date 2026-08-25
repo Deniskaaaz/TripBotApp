93d6bc4b8bab103bdb8b51afae58943e888474b9
Deniskaaaz <www.petuk-denis@yandex.ru>
2026-08-25 08:41:11 +0000

    Remove duplicate route model files

--- app/src/main/java/com/tripbot/app/data/models/RouteRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.tripbot.app.data.models
-
-data class RouteRequest(
-    val origin: String,
-    val destination: String,
-    val city: String
-)
--- app/src/main/java/com/tripbot/app/data/models/RouteResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.tripbot.app.data.models
-
-data class RouteResponse(
-    val distance_km: Double,
-    val duration_sec: Int,
-    val origin_coords: List<Double>,
-    val dest_coords: List<Double>,
-    val map_link: String
-)
